1ec8e4cd41a48c49308f451088829c5439971958
Karen Payne <paynekaren@comcast.net>
2022-06-10 08:57:04 -0700

    one more sample

---
+++ ChunkingConsoleApp/LanguageExtensions/GenericExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkingConsoleApp.LanguageExtensions
+{
+    public static class GenericExtensions
+    {
+
+        /// <summary>
+        /// Create a List of List of T from a List of T split by integer
+        /// </summary>
+        /// <typeparam name="T">Type of List</typeparam>
+        /// <param name="source">List to split</param>
+        /// <param name="chunkSize">Split list by</param>
+        /// <returns>Chunked list</returns>
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize) =>
+            source
+                .Select((value, index) => new { Index = index, Value = value })
+                .GroupBy(item => item.Index / chunkSize)
+                .Select(item => item.Select(value => value.Value).ToList())
+                .ToList();
+    }
+}
---
+++ ChunkingConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChunkingConsoleApp.LanguageExtensions;
+using static System.Globalization.DateTimeFormatInfo;
+namespace ChunkingConsoleApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var chunked = MonthNames().ChunkBy(2);
+            foreach (var item in chunked)
+            {
+                item.ForEach(Console.WriteLine);
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+
+
+        public static List<string> MonthNames() => CurrentInfo!.MonthNames[..^1].ToList();
+    }
+}
