25942b1ecbeb4347c8163f3dbb314669fad8f759
Karen Payne <paynekaren@comcast.net>
2022-06-10 09:37:37 -0700

    refactor

---
+++ Sample2/Models/CountryAndAgeGroups.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2.Models
+{
+    public class CountryAndAgeGroups
+    {
+        public Country HomeCountry { get; }
+        public int Age { get; }
+
+        public CountryAndAgeGroups(Country homeCountry, int age)
+        {
+            HomeCountry = homeCountry;
+            Age = age;
+        }
+    }
+}
--- Sample2/Program.cs
+++ Sample2/Program.cs
@@ -1,29 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Sample2.Classes;
 using Sample2.Models;
 
 namespace Sample2
 {
+
+
     partial class Program
     {
         static void Main(string[] args)
         {
-            var usersGroupedByCountryAndAge = Mocked.Users()
-                .GroupBy(user => new { HomeCountry = user.Country, user.Age });
-
+            var usersGroupedByCountryAndAge = SomeExample();
             foreach (var group in usersGroupedByCountryAndAge)
             {
-                
-                Console.WriteLine($"Users from {group.Key.HomeCountry} at the age of {group.Key.Age}:");
+                Console.WriteLine($"Users from {@group.Key.HomeCountry} at the age of {@group.Key.Age}:");
 
-                foreach (var user in group)
+                foreach (var user in @group)
                 {
                     Console.WriteLine($"\t{user.Name,-20} [{user.Age} years]");
                 }
             }
-
             Console.ReadLine();
         }
+
+        private static IEnumerable<IGrouping<CountryAndAgeGroups, User>> SomeExample() 
+            => Mocked
+                .Users()
+                .GroupBy(user => new CountryAndAgeGroups(user.Country, user.Age));
+
     }
 }
